f5702d49f862aea63014839e214a901db89a79eb
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2020-09-09 15:58:17 +0300

    some tests for find usages

--- web-test2-client/src/main/java/com/mytests/micronaut/testWeb/test2/client/MyClient.java
+++ web-test2-client/src/main/java/com/mytests/micronaut/testWeb/test2/client/MyClient.java
@@ -11,15 +11,27 @@ import io.reactivex.Single;
  * <p>Project: micronaut-web-test2</p>
  * *******************************
  */
-@Client(id="micronaut-web-test2")
+@Client(id = "micronaut-web-test2")
 public interface MyClient {
-    
+
     @Get("/test0")
     Single<String> test0();
-    
-    @Get("/test1/{v1}")
-    Single<String> test1(@PathVariable("v1") String v1);
-    
+
+    @Get("/test1/{myvar1}")
+    Single<String> test1(@PathVariable("myvar1") String v1);
+
     @Get("/test2/{v1}/{v2}")
     Single<String> test2(String v1, String v2);
+
+    @Get("/test3/foo{myvar3}bar")
+    Single<String> test3(@PathVariable("myvar3") String v3);
+
+    @Get("/test3/{myvar3}")
+    Single<String> test31(@PathVariable("myvar3") String v3);
+
+    @Get("/test4/{myvar4}/{myvar3}")
+    Single<String> test4(@PathVariable("myvar3") String v3, @PathVariable("myvar4") String v4);
+
+    @Get("/test5/{myvar3}")
+    Single<String> test5(@PathVariable("myvar3") String v3);
 }
--- web-test2-server/src/main/java/com/mytests/micronaut/testWeb/test2/server/SimpleController1.java
+++ web-test2-server/src/main/java/com/mytests/micronaut/testWeb/test2/server/SimpleController1.java
@@ -15,14 +15,32 @@ public class SimpleController1 {
     
     @Get("/test0")
     public String test0(){
+
         return "simple  test0";
     }
+
     @Get("/test1/{myvar1}")
-    public String test1(@PathVariable("myvar1") String v1){
-        return "simple test1 "+v1;
+    public String test1(@PathVariable("myvar1") String v1) {
+        return "simple test1 " + v1;
     }
+
     @Get("/test2/{myvar1}/{myvar2}")
-    public String test2(@PathVariable("myvar1") String v1, @PathVariable("myvar2") String v2){
-        return "simple test2 "+v1+v2;
+    public String test2(@PathVariable("myvar1") String v1, @PathVariable("myvar2") String v2) {
+        return "simple test2 " + v1 + v2;
+    }
+
+    @Get("test3/foo{myvar3}bar")
+    public String test3(@PathVariable("myvar3") String pv) {
+        return "simple test3 " + pv;
+    }
+
+    @Get("test4/{myvar4}/{myvar3}")
+    public String test4(@PathVariable("myvar3") String pv3, @PathVariable("myvar4") String pv4) {
+        return "simple test4 " + pv3 + pv4;
+    }
+
+    @Get("test5/{myvar3:[a-z]{3}\\d}")
+    public String test5(@PathVariable("myvar3") String pv) {
+        return "simple test5 " + pv;
     }
 }
